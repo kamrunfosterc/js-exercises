25546cace5fcd6ff4052993fbcb1af0a9fbabb3a
Kamrun Foster <kamrun.foster4@gmail.com>
2021-12-03 10:45:39 -0600

    hackerrank java ifElse 3rd problem

---
+++ hackerFile/java_solnProblems/ifElseCondition_3.java
@@ -0,0 +1,105 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+public class ifElseCondition_3 {
+    public class Solution {
+// this checks for 2 conditions, even & odd, and then applies conditions
+        private static final Scanner scanner = new Scanner(System.in);
+
+        public static void main(String[] args) {
+            int N = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            scanner.close();
+            //if n is odd
+            if (N%2== 1){
+                System.out.println("Weird");
+            }
+            // if n is even
+            if (N%2==0){
+                if (N>=2 && N<=5){
+                    //for 2<N<5 conditional
+                    System.out.println("Not Weird");
+                }else if (N>=6 && N<=20){
+                    //for 6<N<20 conditional
+                    System.out.println("Weird");
+                } else{
+                    //for N>20 conditional
+                    System.out.println("Not Weird");
+                }
+                }
+            }
+        }
+        //todo Second option: cleaner code
+    public class secondSoln {
+            public static void main(String[] args) {
+                Scanner s = new Scanner(System.in);// user input
+                String result;// empty declared result value
+                int N = s.nextInt();//declared int variable thats defined as user input
+
+                if(N%2==0){
+                    if (N>=2 && N<=5 || N>20){
+                        result = "Not Weird";
+                    }else {
+                        result = "Weird";
+                    }
+                } else {
+                    result = "Weird";
+                }
+                System.out.println(result);
+            }
+        }
+    }// end of ifElseCondition_3
+
+
+
+//In this challenge, we test your knowledge of using if-else conditional statements to automate decision-making processes. An if-else statement has the following logical flow:
+//
+//        Wikipedia if-else flow chart
+//
+//        Source: Wikipedia
+//
+//        Task
+//        Given an integer, , perform the following conditional actions:
+//
+//        If  is odd, print Weird
+//        If  is even and in the inclusive range of  to , print Not Weird
+//        If  is even and in the inclusive range of  to , print Weird
+//        If  is even and greater than , print Not Weird
+//        Complete the stub code provided in your editor to print whether or not  is weird.
+//
+//        Input Format
+//
+//        A single line containing a positive integer, .
+//
+//        Constraints
+//
+//        Output Format
+//
+//        Print Weird if the number is weird; otherwise, print Not Weird.
+//
+//        Sample Input 0
+//
+//        3
+//        Sample Output 0
+//
+//        Weird
+//        Sample Input 1
+//
+//        24
+//        Sample Output 1
+//
+//        Not Weird
+//        Explanation
+//
+//        Sample Case 0:
+//        is odd and odd numbers are weird, so we print Weird.
+//
+//        Sample Case 1:
+//        and  is even, so it isn't weird. Thus, we print Not Weird.
