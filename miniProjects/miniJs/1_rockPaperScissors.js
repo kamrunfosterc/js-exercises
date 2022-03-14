46278f6fff4d7eef728fa0f2d1570222e69a4b46
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-14 11:12:42 -0500

    building mini rock paper scissors game

---
+++ miniProjects/miniJs/1_rockPaperScissors.js
@@ -0,0 +1,7 @@
+'use strict'
+// grab and store element by the id for use later
+const cpuChoice = document.getElementById('computer-choice');
+const userChoice = document.getElementById('user-choice');
+const genResult = document.getElementById('result');
+
+
