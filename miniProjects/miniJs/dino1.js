ecdbaf13f008fcb18bca737ad5a479f91d4c9819
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-13 21:50:32 -0500

    completed phase 1 of the dino game

--- miniProjects/miniJs/dino1.js
+++ miniProjects/miniJs/dino1.js
@@ -6,8 +6,25 @@ let block = document.getElementById("block");
 
 //function to get character to be interactive
 function jump(){
-    character.classList.add("jumpAnimate");
-    setTimeout(function (){}, 500);//this adds in the animation and reloads after 500ms so it can run again
+    if (character.classList != "jumpAnimate"){
+        character.classList.add("jumpAnimate");
+    }
+    setTimeout(function (){
+        character.classList.remove("jumpAnimate")
+    }, 500);//this adds in the animation and reloads after 500ms so it can run again
 }
 // onclick(jump()) could've used an event listener here as well
+// $(selector).click()
+
+// function that runs every so often to see if you've lost or not
+let checkDead = setInterval(function (){
+    // get top position of character and left position of block
+    var characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    var blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if (blockLeft<20 && blockLeft>0 && characterTop>=130){
+        block.style.animation = 'none';
+        block.style.display = 'none';
+        alert('OH NO! YOU LOSE!')
+    }
+    },10)
 
