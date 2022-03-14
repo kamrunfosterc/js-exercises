aa0d188a2a3e1d7867f7e266376b3297b6e14a3b
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-14 12:32:29 -0500

    updated rps game

--- miniProjects/miniJs/1_rockPaperScissors.js
+++ miniProjects/miniJs/1_rockPaperScissors.js
@@ -1,7 +1,37 @@
 'use strict'
 // grab and store element by the id for use later
-const cpuChoice = document.getElementById('computer-choice');
-const userChoice = document.getElementById('user-choice');
-const genResult = document.getElementById('result');
+const cpuChoiceDisplay = document.getElementById('computer-choice');
+const userChoiceDisplay = document.getElementById('user-choice');
+const genResultDisplay = document.getElementById('result');
 
+/*using a query selector to create possible choices*/
+const possibleChoice = document.querySelectorAll('button');
 
+// global storing variable
+let userChoice;
+let computerChoice;
+
+/*4 each possible choice, grabbing the possible choice and listen for event click*/
+possibleChoice.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    /*this grabs event and displays user choice on html page in corresponding id section*/
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+}));
+
+function generateComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * possibleChoice.length) +1; //possibleChoice.length = 3 in this case, can sub
+    //added 1 so that user doesn't get a 0 assoc w. random number
+    // console.log(randomNumber);
+
+    if (randomNumber === 1){
+        computerChoice = 'Rock';
+    }
+    if (randomNumber === 2){
+        computerChoice = 'Scissors';
+    }
+    if (randomNumber === 3){
+        computerChoice = 'Paper';
+    }
+    cpuChoiceDisplay.innerHTML = computerChoice;
+}
