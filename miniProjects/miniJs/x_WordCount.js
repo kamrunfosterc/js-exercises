2c4316abb6aaa4337241c0bc6ea11e8f450b0429
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-15 21:43:41 -0500

    updating info

--- miniProjects/miniJs/x_WordCount.js
+++ miniProjects/miniJs/x_WordCount.js
@@ -1,1 +1,23 @@
 'use strict'
+let countTarget = document.querySelector("word-count-input");
+let wordCount = document.querySelector("#word-count");
+let characterCount = document.querySelector("#character-count");
+let count = function(){
+    let characters = countTarget.value;
+    let characterLength = characters.length;
+    let words = characters.split(/[\n\r\s]+/g).filter(function (word){
+        return word.length > 0;
+    });
+    wordCount.innerHTML = words.length;
+    characterCount.innerHTML = characterLength;
+};
+count();
+window.addEventListener(
+    "input",
+    function(e){
+        if (e.target.matches("#word-count-input")){
+            count();
+        }
+    },
+    false
+);
