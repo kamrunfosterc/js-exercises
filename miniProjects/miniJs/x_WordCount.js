9276388e5a5e20ebd4b09e504096eddb13a6099d
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-15 23:18:08 -0500

    fixed word count issue with javascript

--- miniProjects/miniJs/x_WordCount.js
+++ miniProjects/miniJs/x_WordCount.js
@@ -1,14 +1,16 @@
 'use strict'
-let countTarget = document.querySelector("word-count-input");
+let countTarget = document.querySelector("#word-count-input");
 let wordCount = document.querySelector("#word-count");
 let characterCount = document.querySelector("#character-count");
+
 let count = function(){
     let characters = countTarget.value;
-    let characterLength = characters.length;
+    let characterLength = characters.length;// error found her when using .length()
     let words = characters.split(/[\n\r\s]+/g).filter(function (word){
         return word.length > 0;
     });
     wordCount.innerHTML = words.length;
+    console.log(wordCount);
     characterCount.innerHTML = characterLength;
 };
 count();
