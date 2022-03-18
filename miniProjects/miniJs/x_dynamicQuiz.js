41724c213c32c543f11c3021242001dd9a9a2cbd
Kamrun Foster <kamrun.foster4@gmail.com>
2022-03-18 09:32:38 -0500

    working on dynamic quiz css/ js portion of project

---
+++ miniProjects/miniJs/x_dynamicQuiz.js
@@ -0,0 +1,98 @@
+// 'use strict'
+
+(function (){
+    var questions = [{
+        question: "What is 2*5?",
+        choices: [2,5,10,15,20],
+        correctAnswer: 2
+    },{
+        question:"What is 3*6?",
+        choices:[3,6,9,12,18],
+        correctAnswer: 4
+    },{
+        question:"What is 8*9?",
+        choices:[72,99,108,134,156],
+        correctAnswer: 0
+    },{
+        question:"What is 8*8?",
+        choices: [20,30,40,50,64],
+        correctAnswer: 4
+    }];
+    var questionCounter = 0;//this tracks question number
+    var selections = [];//array containing user choices
+    var quiz = $('#quiz');//jquery call on quiz div object
+
+    // Display initial question
+    displayNext();
+
+    // Click handler for the 'next' button
+    $('#next').on('click', function (e){
+        e.preventDefault();
+
+        // Suspend click listener during fade animation
+        if(quiz.is(':animated')){
+            return false;//won't work during transition between questions
+        }
+        choose();
+
+        // If no user selection, progress is stopped
+        if(isNaN(selections[questionCounter])){
+            alert('Please make a selection!');
+        } else{
+            questionCounter++;
+            displayNext();
+        }
+    });
+    //Click handler for the 'prev' button
+    $('#prev').on('click', function (e){
+        e.preventDefault();
+
+        if (quiz.is(':animated')){
+            return false;
+        }
+        choose();
+        questionCounter--;
+        displayNext();
+    });
+    //Click handler for the 'Start Over' button
+    $('#start').on('click', function (e){
+        e.preventDefault();
+
+        if (quiz.is(':animated')){
+            return false;
+        }
+        questionCounter = 0;
+        selections = [];
+        displayNext();
+        $('#start').hide();
+    });
+
+    //Animates buttons on hover
+    $('.button').on('mouseenter', function(){
+        $(this).addClass('active');
+    });
+    $('.button').on('mouseleave', function (){
+        $(this).removeClass('active');
+    });
+
+    //Creates and returns div containing questions and answer selections
+    function createQuestionElement(index){
+        var qElement = $('<div>',{
+            id: 'question'
+        });
+        var header = $('<h2>Question '+ (index + 1)+ ':</h2>');
+        qElement.append(header);
+
+        var question = $('<p>').append(questions[index].question);
+        qElement.append(question);
+
+        var radioButtons = createRadios(index);
+        qElement.append(radioButtons);
+
+        return qElement;
+    }
+
+    //Creates list of answer choices as radio inputs
+
+
+})
